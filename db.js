5c31d00a0be6ffc48358b89039e0ab201212021d
Qi Feng Huang <qifenghuang2@gmail.com>
2017-06-29 23:25:31 -0400

    t

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-var db = !process.env.db ? 'mongodb://localhost:27017/user' : 'mongodb://master:1q2wqawsazsx@ds017175.mlab.com:17175/user';
+var db = !process.env.db ? 'mongodb://localhost:27017/user' : process.env.db;
 
 mongoose.connect(db);
 
