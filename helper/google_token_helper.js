9e34b4bdd3b5369e50fada12a8bf98d1788d3973
Qi Feng Huang <qifenghuang2@gmail.com>
2017-07-06 00:26:20 -0400

    added logs

--- helper/google_token_helper.js
+++ helper/google_token_helper.js
@@ -8,6 +8,9 @@ var client = new auth.OAuth2(configs.google_client_id, '', '');
 
 // callback (err, loginInfo)
 function verify(token, callback) {
+  console.log("token "  + token);
+  console.log("client: " + configs.google_client_id);
+  console.log("callback", + !callback);
   client.verifyIdToken( 
     token,
     configs.google_client_id,
